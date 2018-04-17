c25fdd1681a37346429b32cbf1203e8051acc1b4
Oleksandr Borysenko <oleksandr.borysenko@idapgroup.com>
2018-04-17 16:42:13 +0300

    Initial commit

---
+++ GoogleMapsTestApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GoogleMapsTestApp
+//
+//  Created by Oleksandr Borysenko on 4/17/18.
+//  Copyright © 2018 Oleksandr Borysenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
